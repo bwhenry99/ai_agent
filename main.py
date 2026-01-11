b1e8d438a27336d3baf9dc5e931e4e94cb64814f
bwhenry <bwhenry19@gmail.com>
2026-01-11 12:48:59 -0700

    retrieve response matadata

--- main.py
+++ main.py
@@ -7,9 +7,13 @@ def main():
     api_key = os.environ.get("GEMINI_API_KEY")
 
     client = genai.Client(api_key=api_key)
+    prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
 
-    response = client.models.generate_content(model="gemini-2.5-flash", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
-    print(response.text)
+    response = client.models.generate_content(model="gemini-2.5-flash", contents=prompt)
+    print(f"User prompt: {prompt}")
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    print(f"Resonpse: {response.text}")
 
 
 if __name__ == "__main__":
