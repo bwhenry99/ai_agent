641dc233efb7056f044b31ff14156319eb2dbe46
bwhenry <bwhenry19@gmail.com>
2026-01-11 13:11:27 -0700

    verbose output

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 import argparse
 
 def main():
@@ -9,15 +10,18 @@ def main():
 
     parser = argparse.ArgumentParser(description="Chatbot")
     parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
 
     client = genai.Client(api_key=api_key)
     prompt = args.user_prompt
+    messages = [types.Content(role="user", parts=[types.Part(text=prompt)])]
 
-    response = client.models.generate_content(model="gemini-2.5-flash", contents=prompt)
-    print(f"User prompt: {prompt}")
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    response = client.models.generate_content(model="gemini-2.5-flash", contents=messages)
+    if(args.verbose):
+        print(f"User prompt: {prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
     print(f"Resonpse: {response.text}")
 
 
